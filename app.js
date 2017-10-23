a8e7465701055197930524828287ac0410730ee6
Unknown <pe0877888@otc.edu>
2017-10-22 19:30:53 -0500

    Step2

--- app.js
+++ app.js
@@ -36,4 +36,39 @@ $(document).ready(function () {
         // Add one attendee by default on init
         addAttendee();
         syncPurchaseButton();
+        function addAttendee() {
+            $('.attendee-list').append(
+                $('script[data-template="attendee"]').text()
+            );
+        
+            // Sync remove button UI
+            syncRemoveButtons();
+        }
+        
+        function syncRemoveButtons() {
+            // If only one attendee, hide the first remove button
+            // otherwise, show all remove buttons
+            if (getAttendeeCount() === 1) {
+                $('.attendee-list .attendee .remove-attendee').first().hide();
+            } else {
+                $('.attendee-list .attendee .remove-attendee').show();
+            }
+        }
+        
+        function syncPurchaseButton() {
+            // Total up the count for the checkout button total
+            $('#checkout-button span.amount').html(
+                '$' + data.cost * getAttendeeCount()
+            );
+        }
+        
+        // Events
+        $('.add-attendee').on('click', function (event) {
+            event.preventDefault();
+            addAttendee();
+            $(this).trigger('attendee:add');
+        }).on('attendee:add', function () {
+            syncPurchaseButton();
+            syncRemoveButtons();
+        });
     });
